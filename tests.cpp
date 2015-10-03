79c6fb7b60feffaf72901fe4420a936b54bb54fb
marina <marynakuzionka@ukr.net>
2015-10-04 00:42:00 +0300

    add tests

---
+++ tests.cpp
@@ -0,0 +1,14 @@
+//
+// Created by marina on 04.10.15.
+//
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE LZW_tests
+
+#include <boost/test/unit_test.hpp>
+#include "lzw.h"
+
+
+BOOST_AUTO_TEST_CASE(testing_LZW_class) {
+
+}

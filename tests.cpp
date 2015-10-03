1f5879c2bfd96b05e6d328815916750b71f9db4c
marina <marynakuzionka@ukr.net>
2015-10-04 02:45:09 +0300

    add tests

--- tests.cpp
+++ tests.cpp
@@ -8,7 +8,22 @@
 #include <boost/test/unit_test.hpp>
 #include "lzw.h"
 
-
 BOOST_AUTO_TEST_CASE(testing_LZW_class) {
+    LZW lzw;
+    string inp = "a";
+    istringstream is(inp);
+    ostringstream os;
+    lzw.compress(is,os);
+
+    istringstream is2(os.str());
+    ostringstream os2;
+    lzw.decompress(is2,os2);
+    BOOST_CHECK_EQUAL(inp,os2.str());
+    cout << os2.str();
+
+    std::map<std::vector<char>, CodeType> dictionary;
+    lzw.reset_dictionary_compress(dictionary);
+    //BOOST_CHECK_EQUAL(dictionary[],0);
+
 
 }
